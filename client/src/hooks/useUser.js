88bc23c732e304a7493a38d82f46823ed1e37851
kevinnail <fumalicious@gmail.com>
2025-09-24 10:36:10 -0700

    format, remove old code

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,28 +1,15 @@
 import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext.js';
 import { authUser } from '../services/auth.js';
-import process from 'process';
 
 export function useUser() {
-  const {
-    user,
-    setUser,
-    loading,
-    setLoading,
-    error,
-    setError,
-    userId,
-    setUserId,
-  } = useContext(UserContext);
+  const { user, setUser, loading, setLoading, error, setError } =
+    useContext(UserContext);
 
   const logInUser = async (email, password, type) => {
     const user = await authUser(email, password, type);
-
     setUser(user);
-    setUserId(process.env.REACT_APP_GOOGLE_USER_ID);
     setLoading(false);
-
-    return user;
   };
 
   return {
@@ -33,7 +20,5 @@ export function useUser() {
     logInUser,
     loading,
     setLoading,
-    userId,
-    setUserId,
   };
 }
