c524e2b35364bca27f10733f0c9ef4eb456d8c9c
kevinnail <fumalicious@gmail.com>
2025-09-24 14:22:36 -0700

    fix eslint errors

--- server/lib/app.js
+++ server/lib/app.js
@@ -9,20 +9,20 @@ import errorHandler from './middleware/error.js';
 const app = express();
 
 app.use(
-    cors({
-      origin: ['http://localhost:3000',],
-      credentials: true,
-      // exposedHeaders: ['Mcp-Session-Id'],
-      // allowedHeaders: ['Content-Type', 'Mcp-Session-Id'],
-    }),
-  );
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    // exposedHeaders: ['Mcp-Session-Id'],
+    // allowedHeaders: ['Content-Type', 'Mcp-Session-Id'],
+  })
+);
 
-  app.use(express.json());
-  app.use(cookieParser());
-  
-  // Use the imported routers
-  app.use('/api/v1/users', users);
-  app.use('/api/v1/api', testApi);
+app.use(express.json());
+app.use(cookieParser());
+
+// Use the imported routers
+app.use('/api/v1/users', users);
+app.use('/api/v1/api', testApi);
 
 // Error handling & 404 middleware for when
 // a request doesn't match any app routes
