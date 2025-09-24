44fb9780e73e3151eba0d5993c614e6fe520a32a
kevinnail <fumalicious@gmail.com>
2025-09-24 09:10:34 -0700

    fix test file

--- client/src/components/AuthTest/AuthTest.js
+++ client/src/components/AuthTest/AuthTest.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useUser } from '../../hooks/useUser.js';
+import { signOutUser } from '../../services/fetch-auth.js';
 import { toast } from 'react-toastify';
 import './AuthTest.css';
 
 export default function AuthTest() {
-  const { user, logOutUser } = useUser();
+  const { user, setUser, setUserId } = useUser();
 
   const handleSignOut = async () => {
     try {
-      await logOutUser();
+      await signOutUser();
+      setUser(null);
+      setUserId(null);
       toast.success('Successfully signed out!');
     } catch (error) {
       toast.error('Sign out failed.');
