bbe7575e93a9af4486697d83dfef6ca21da3b3f1
kevinnail <fumalicious@gmail.com>
2025-09-24 09:11:08 -0700

    fix dotenv import

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import app from './lib/app.js';
 
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
-
 // Only start server if not in test environment
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
